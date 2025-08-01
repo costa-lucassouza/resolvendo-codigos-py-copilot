c43a4724059a901941d5d6f136c4216307bd51c4
Lucas <lucassouzacosta0001@gmail.com>
2025-08-01 06:35:37 -0300

    Update concat_dados.py

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,7 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+txt1 = input("Insira a primeira palavra: ")
+txt2 = input("Insira a segunda palavra: ")
+
+txt_concatenado = txt1 + " " + txt2
+
+print("A palavra concatenada é: ",txt_concatenado)
